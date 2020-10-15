debe5e04419fa8194195ff4b8e69001adc83c9d3
Kemiazhuk <garadenets@gmail.com>
2020-10-15 17:32:30 +0200

    commmit mess 1

--- src/Main.java
+++ src/Main.java
@@ -2,40 +2,40 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        LinearPrograms1 prog1 = new LinearPrograms1();
-//        System.out.println("z = " + prog1.calc());
-//
-//        LinearPrograms2 prog2 = new LinearPrograms2();
-//        System.out.println("math expression is " + prog2.calc());
-//
-//        LinearPrograms3 prog3 = new LinearPrograms3();
-//        System.out.println("math expression is " + prog3.calc());
-//
-//        LinearPrograms4 prog4 = new LinearPrograms4();
-//        System.out.println("math expression is " + prog4.calc());
-//
-//        LinearPrograms5 prog5 = new LinearPrograms5();
-//        System.out.println("time is " + prog5.calc());
-//
-//        LinearPrograms6 prog6 = new LinearPrograms6();
-//        System.out.println(prog6.calc());
+        LinearPrograms1 prog1 = new LinearPrograms1();
+        System.out.println("z = " + prog1.calc());
 
-//        Branching1 prog7 =new Branching1();
-//        boolean e =prog7.existenceTriangle();
-//        boolean r =prog7.rightTriangle();
-//        if ((e)&&(r)){
-//            System.out.println("Triangle exist and right");
-//        }else  if ((e)&&(!r)){
-//            System.out.println("Triangle exist but not right");
-//        }else System.out.println("Triangle does't exist");
-//        Branching2 prog8 =new Branching2();
-//        System.out.println("max(min(a,b),min(c,d))"+prog8.max());
-//        Branching3 prog9 = new Branching3();
-//        if (prog9.belongLine()) {
-//            System.out.println("3 points belong to the same line");
-//        } else {
-//            System.out.println("3 points do not belong to the same line" );
-//        }
+        LinearPrograms2 prog2 = new LinearPrograms2();
+        System.out.println("math expression is " + prog2.calc());
+
+        LinearPrograms3 prog3 = new LinearPrograms3();
+        System.out.println("math expression is " + prog3.calc());
+
+        LinearPrograms4 prog4 = new LinearPrograms4();
+        System.out.println("math expression is " + prog4.calc());
+
+        LinearPrograms5 prog5 = new LinearPrograms5();
+        System.out.println("time is " + prog5.calc());
+
+        LinearPrograms6 prog6 = new LinearPrograms6();
+        System.out.println(prog6.calc());
+
+        Branching1 prog7 =new Branching1();
+        boolean e =prog7.existenceTriangle();
+        boolean r =prog7.rightTriangle();
+        if ((e)&&(r)){
+            System.out.println("Triangle exist and right");
+        }else  if ((e)&&(!r)){
+            System.out.println("Triangle exist but not right");
+        }else System.out.println("Triangle does't exist");
+        Branching2 prog8 =new Branching2();
+        System.out.println("max(min(a,b),min(c,d))"+prog8.max());
+        Branching3 prog9 = new Branching3();
+        if (prog9.belongLine()) {
+            System.out.println("3 points belong to the same line");
+        } else {
+            System.out.println("3 points do not belong to the same line" );
+        }
         Branching4 prog10 = new Branching4();
         System.out.println(prog10.calc());
 
