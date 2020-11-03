42550145d6df4f514d1848d2f99b199592a23677
User <rr>
2020-11-03 04:25:14 +0300

    rrr

---
+++ src/org/kemy/Test.java
@@ -0,0 +1,28 @@
+//package org.kemy;
+//
+//import java.util.ArrayList;
+//
+//public class Test {
+//    public static void main(String[] args){
+//        ArrayList<Integer> newList = new ArrayList<Integer>();
+//        newList.add(5);
+//        newList.add(10);
+//        System.out.println(newList.get(0));
+////    }
+////}
+//public int nums(double num) {
+//        int i = 0;
+//        int count = 0;
+//        String s = String.valueOf(num);
+//        while (i < s.length()) {
+//        if (s.charAt(i) == '.') {
+//        count = s.length() - 1;
+//        break;
+//        }
+//        i++;
+//        }
+//        if (count == 0) {
+//        return i;
+//        } else {
+//        return count;
+//        }
