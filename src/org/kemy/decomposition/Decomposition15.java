29c3848d9eb6efb75eceb35d53d353a25be94785
User <rr>
2020-11-04 22:34:59 +0300

    rrr

--- src/org/kemy/decomposition/Decomposition15.java
+++ src/org/kemy/decomposition/Decomposition15.java
@@ -10,25 +10,25 @@ public class Decomposition15 {
         ArrayList<Integer> allNums = new ArrayList<>();
         System.out.println("Enter N - how many digits ");
         int n = Input.inputInt();
-        int sum = 0;
         int item[] = new int[n];
-        allNums = combinations(allNums, item, 0,1);
+        allNums = combinations(allNums, item, 0, 1);
         for (int i = 0; i < allNums.size(); i++) {
-           System.out.println(allNums.get(i));
+            System.out.println(allNums.get(i));
         }
     }
+
     public ArrayList<Integer> combinations(ArrayList<Integer> allCombinations, int[] item, int count, int start) {
         int num = 0;
         if (count < item.length) {
             for (int i = start; i < 10; i++) {
                 item[count] = i;
-                combinations(allCombinations, item, count + 1, i+1);
+                combinations(allCombinations, item, count + 1, i + 1);
             }
         } else {
-                for (int i = 0; i < item.length; i++) {
-                    num = num * 10 + item[i];
-                }
-                allCombinations.add(num);
+            for (int i = 0; i < item.length; i++) {
+                num = num * 10 + item[i];
+            }
+            allCombinations.add(num);
         }
         return allCombinations;
     }
