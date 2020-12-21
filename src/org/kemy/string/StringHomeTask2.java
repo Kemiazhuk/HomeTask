0a32624a70c63d808334e9a95c6c6da1119a043d
User <rr>
2020-12-21 20:48:50 +0300

    new

--- src/org/kemy/string/StringHomeTask2.java
+++ src/org/kemy/string/StringHomeTask2.java
@@ -3,7 +3,7 @@ package org.kemy.string;
 import org.kemy.Input;
 
 public class StringHomeTask2 {
-    public void task() {
+    public static void main(String[] args) {
 
         System.out.println("enter string");
         String str = new String(Input.inputStr());
@@ -33,6 +33,29 @@ public class StringHomeTask2 {
             }
             i++;
         }
+        if (j > 0)
+            newStr.append(str.subSequence(i - j, i));
+//        for (i = 0; i < str.length(); ++i) {
+//            // current symbol in string matches next symbol in "word"
+//            if (str.charAt(i) == wordToChange.charAt(j)) {
+//                j++;
+//                if (wordToChange.length() == j) {
+//                    // entire word found -> replace
+//                    newStr.append(wordToAdd);
+//                    j = 0;
+//                }
+//            } else {
+//                if (j > 0) {
+//                    newStr.append(str.subSequence(i - j, i));
+//                    j = 0;
+//                }
+//
+//                newStr.append(str.charAt(i));
+//            }
+//        }
+//        if (j > 0)
+//            newStr.append(str.subSequence(i - j, i));
         System.out.println(newStr);
+
     }
 }
